937c98a7453a4d48a3726b4636effcf2c09bc1f3
shridhaarchippaa <shridhaar.chippaa@accenture.com>
2017-04-20 14:49:20 +0100

    removed pro-rata logic from input and output model

--- app/models/output/tc/TCCalculation.scala
+++ app/models/output/tc/TCCalculation.scala
@@ -29,22 +29,22 @@ object TCCalculation extends utils.CCFormat {
   implicit val TCCalculationWrites: Writes[TCCalculation] = (
     (JsPath \ "from").write[LocalDate](jodaLocalDateWrites(datePattern)) and
       (JsPath \ "until").write[LocalDate](jodaLocalDateWrites(datePattern)) and
-        (JsPath \ "proRataEnd").writeNullable[LocalDate](jodaLocalDateWrites(datePattern)) and
+//        (JsPath \ "proRataEnd").writeNullable[LocalDate](jodaLocalDateWrites(datePattern)) and
           (JsPath \ "totalAwardAmount").write[BigDecimal] and
-            (JsPath \ "totalAwardProRataAmount").write[BigDecimal] and
+//            (JsPath \ "totalAwardProRataAmount").write[BigDecimal] and
               (JsPath \ "houseHoldAdviceAmount").write[BigDecimal] and
-               (JsPath \ "totalHouseHoldAdviceProRataAmount").write[BigDecimal] and
+//               (JsPath \ "totalHouseHoldAdviceProRataAmount").write[BigDecimal] and
                   (JsPath \ "taxYears").write[List[TaxYear]]
     )(unlift(TCCalculation.unapply))
 }
 
 case class TCCalculation(from: LocalDate,
                          until: LocalDate,
-                         proRataEnd: Option[LocalDate] = None,
+//                         proRataEnd: Option[LocalDate] = None,
                          totalAwardAmount: BigDecimal = 0.00,
-                         totalAwardProRataAmount: BigDecimal = 0.00,
+//                         totalAwardProRataAmount: BigDecimal = 0.00,
                          houseHoldAdviceAmount: BigDecimal = 0.00,
-                         totalHouseHoldAdviceProRataAmount: BigDecimal = 0.00,
+//                         totalHouseHoldAdviceProRataAmount: BigDecimal = 0.00,
                          taxYears: List[TaxYear]
                           )
 
@@ -52,11 +52,11 @@ object TaxYear extends utils.CCFormat {
   implicit val TaxYearWrites: Writes[TaxYear] = (
     (JsPath \ "from").write[LocalDate](jodaLocalDateWrites(datePattern)) and
       (JsPath \ "until").write[LocalDate](jodaLocalDateWrites(datePattern)) and
-      (JsPath \ "proRataEnd").writeNullable[LocalDate](jodaLocalDateWrites(datePattern)) and
+//      (JsPath \ "proRataEnd").writeNullable[LocalDate](jodaLocalDateWrites(datePattern)) and
         (JsPath \ "taxYearAwardAmount").write[BigDecimal] and
-          (JsPath \ "taxYearAwardProRataAmount").write[BigDecimal] and
+//          (JsPath \ "taxYearAwardProRataAmount").write[BigDecimal] and
             (JsPath \ "taxYearAdviceAmount").write[BigDecimal] and
-      (JsPath \ "taxYearAdviceProRataAmount").write[BigDecimal] and
+//              (JsPath \ "taxYearAdviceProRataAmount").write[BigDecimal] and
               (JsPath \ "periods").write[List[Period]]
     )(unlift(TaxYear.unapply))
 }
@@ -64,11 +64,11 @@ object TaxYear extends utils.CCFormat {
 case class TaxYear(
                     from: LocalDate,
                     until: LocalDate,
-                    proRataEnd: Option[LocalDate] = None,
+//                    proRataEnd: Option[LocalDate] = None,
                     taxYearAwardAmount: BigDecimal = BigDecimal(0.00),
-                    taxYearAwardProRataAmount: BigDecimal = BigDecimal(0.00),
+//                    taxYearAwardProRataAmount: BigDecimal = BigDecimal(0.00),
                     taxYearAdviceAmount: BigDecimal = BigDecimal(0.00),
-                    taxYearAdviceProRataAmount: BigDecimal = BigDecimal(0.00),
+//                    taxYearAdviceProRataAmount: BigDecimal = BigDecimal(0.00),
                     periods: List[Period]
                     )
 
