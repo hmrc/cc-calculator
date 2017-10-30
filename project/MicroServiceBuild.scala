4455a7524a91e8d758b1032710039ecae9551536
shridhaarchippaa <shridhaar.chippaa@accenture.com>
2017-10-30 22:24:46 +0000

    Upgraded bootstrap version to 6.12.0

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -12,10 +12,10 @@ private object AppDependencies {
   import play.sbt.PlayImport._
   import play.core.PlayVersion
 
-  private val microServiceBootstrapVersion = "6.9.0"
+  private val microServiceBootstrapVersion = "6.12.0"
   private val scalaTestVersion = "2.2.6"
   private val pegDownVersion = "1.6.0"
-  private val hmrcTestVersion = "2.3.0"
+  private val hmrcTestVersion = "2.4.0"
   private val jsonSchemaValidator = "2.2.6"
   private val json4s = "3.2.11"
   private val mockitoVersion = "1.9.0"
