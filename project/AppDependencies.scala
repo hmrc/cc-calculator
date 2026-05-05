3e8708756d55f5633e35b29209946726e2802250
Mathew Joseph <183598333+matjoseph24@users.noreply.github.com>
2026-05-05 12:27:46 +0100

    DL-19091: Dependency upgrade (#105)
    * DL-19091: Dependency upgrade
    
    * DL-19091: Update sbt and scalafmt versions
    
    * DL-19091: Upgrade jackson dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import play.sbt.PlayImport._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.11.0"
+  private val bootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapVersion,
     "com.github.java-json-tools"    % "json-schema-validator"     % "2.2.14",
-    "org.json4s"                   %% "json4s-jackson"            % "4.1.0-M4",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.16.1"
+    "org.json4s"                   %% "json4s-jackson"            % "4.1.0-M8",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.21.3"
   )
 
   val test: Seq[ModuleID] = Seq(
